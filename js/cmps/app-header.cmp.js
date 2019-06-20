06e24d0d255c54b96c7e9b3bb3a7832472beb479
Ron Avnery <avnery@gmail.com>
2019-06-20 14:23:14 +0300

    minor design changes

--- js/cmps/app-header.cmp.js
+++ js/cmps/app-header.cmp.js
@@ -2,7 +2,7 @@
 export default {
     template: `
     <section class="app-header">
-        This is the header
+        Logo
         <router-link to="/">Home Page</router-link>
         <router-link to="/mail">Mail App</router-link>
         <router-link to="/note">Note App</router-link>
