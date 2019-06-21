86b55e5d7d7df8bb3c0289e7076c90934a4c876e
Ron Avnery <avnery@gmail.com>
2019-06-21 16:17:27 +0300

    updated background img path

--- js/apps/mail/services/mail-service.cmp.js
+++ js/apps/mail/services/mail-service.cmp.js
@@ -3,6 +3,7 @@
 
 let mailsDB;
 let gNextId = 1;
+let gNextSentTime = Date.now();
 
 export const mailService = {
     queryMails,
@@ -24,18 +25,18 @@ function generateMails() {
     const numOfMails = 10;
     for (let i = 0; i < numOfMails; i++) {
         mails.push(createMail('Ron Avnery',`Subject mothafucka! ${gNextId}`,
-         'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur porro cum placeat pariatur, quia nam id aspernatur repellat quaerat facere nesciunt vitae quidem itaque! Veritatis suscipit quia voluptates quos minima?'))
+         'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur porro cum placeat pariatur, quia nam id aspernatur repellat quaerat facere nesciunt vitae quidem itaque! Veritatis suscipit quia voluptates quos minima?', gNextSentTime-(i*100000000)))
     }
     return mails;
 }
 
-function createMail(sender, subject, body) {
+function createMail(sender, subject, body, sentAt = Date.now()) {
     return {
         id: gNextId++,
         sender,
         subject,
         body,
-        sentAt: Date.now(),
+        sentAt,
         isRead: false,
         isStarred: false,
         isTrashed: false,
@@ -44,7 +45,7 @@ function createMail(sender, subject, body) {
 
 function parseMail(mail, key, length = 50) {
     var item = mail[key]
-    if (key === 'sentAt') return Date(item).toString().split(' ').slice(0, 5).join(' ');
+    if (key === 'sentAt') return moment(item).fromNow();
     if (item.length > length) return item.substring(0,length) + '...'
     return item;
 }
