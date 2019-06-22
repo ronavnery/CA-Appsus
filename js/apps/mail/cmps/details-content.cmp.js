1bb0785d2d2fca8cc96837861592f511107593c5
Ron Avnery <avnery@gmail.com>
2019-06-22 20:28:19 +0300

    Mail: improved mail details & added email address

--- js/apps/mail/cmps/details-content.cmp.js
+++ js/apps/mail/cmps/details-content.cmp.js
@@ -4,14 +4,21 @@ import eventBus, { SHOW_DETAILS } from '../../../event-bus.js';
 export default {
     template: `
     <section v-if="mail" class="details-content-container flex column">
-    <span>{{getMailData('sender')}}</span>
-    <span>{{getMailData('subject')}}</span>
+    <div class="content-sender flex align-center">
+        <div class="content-sender-avatar">{{getMailData('sender').substring(0,1)}}</div>
+        <div class="content-sender-details flex column">
+            <span class="content-sender-name">{{getMailData('sender')}}</span>
+            <span class="content-sender-email">{{getMailData('address')}}</span>
+        </div>
+        <span class="content-time align-end">{{getMailData('sentAt')}}</span>
+    </div>
+    <span class="content-subject">{{getMailData('subject')}}</span>
     <span>{{getMailData('body')}}</span>
+
     <span>isRead: {{getMailData('isRead')}}</span>
     <span>isSent: {{getMailData('isSent')}}</span>
     <span>isStarred: {{getMailData('isStarred')}}</span>
     <span>isTrashed: {{getMailData('isTrashed')}}</span>
-
     </section>
     `,
     data() {
