da689d76446b31cce10de5579e62f0d9f38d94ae
Ron Avnery <avnery@gmail.com>
2019-06-24 18:45:44 +0300

    Header: added nav

--- js/sharedCmps/app-header.cmp.js
+++ js/sharedCmps/app-header.cmp.js
@@ -1,14 +1,43 @@
 
 export default {
     template: `
-    <section class="app-header flex">
+    <section class="app-header flex space-between">
         <div class="header-logo-container">
             <div class="header-logo"></div>
         </div>
-        <router-link to="/">Home Page</router-link>
-        <router-link to="/mail">Mail App</router-link>
-        <router-link to="/note">Note App</router-link>
-        <router-link to="/about">About page</router-link>
+        <div class="flex">
+
+        <img class="main-notification-icon" src="img/logo/notification-logo.svg"/>
+        <i class="icon icon-navigation-menu-1 main-nav-icon" @click="isModalOpen = !isModalOpen"></i>
+        </div>
+        <div class="main-nav-modal flex column align-center" v-if="isModalOpen"
+        >
+
+<div>
+        <router-link to="/mail">
+        <img class="modal-icon" src="img/logo/mail-logo.svg" @click="isModalOpen = !isModalOpen"/>
+        </router-link>
+
+        <router-link to="/note">
+        <img class="modal-icon" src="img/logo/notes-logo.svg" @click="isModalOpen = !isModalOpen"/>
+        </router-link>
+
+</div>
+<div>
+        <router-link to="/">
+        <img class="modal-icon" src="img/logo/home-logo.svg" @click="isModalOpen = !isModalOpen"/>
+        </router-link>
+        <router-link to="/about">
+            <img class="modal-icon" src="img/logo/about-logo.svg" @click="isModalOpen = !isModalOpen"/>
+        </router-link>
+</div>
+        </div>
+
     </section>
-    `
+    `,
+    data() {
+        return {
+            isModalOpen: false
+        }
+    }
 }
