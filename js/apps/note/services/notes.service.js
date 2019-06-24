a3dbb64b466ff6945dd7a40f3c25094614fba44a
Yonatan Galili <yon.galili@gmail.com>
2019-06-24 17:34:51 +0300

    todo lists

--- js/apps/note/services/notes.service.js
+++ js/apps/note/services/notes.service.js
@@ -8,10 +8,10 @@ export default {
   query,
   addNote,
   setValue,
-  setNote,
   deleteNote,
   togglePinned,
-  toggleTodo
+  toggleTodo,
+  updateNote
 }
 
 var gNotes = []
@@ -24,17 +24,13 @@ if (utilService.loadFromStorage('notes')) {
 // console.log('gNotes :', gNotes)
 
 function query() {
-//   gNotes.sort((a, b) => {
-//     // true values first
-//     return (a.pinned === b.pinned)? 0 : a? -1 : 1;
-// });
   utilService.saveToStorage('notes', gNotes)
   return gNotes
 }
 
 function addNote(note) {
   note.id = makeId()
-  // console.log('note :', note)
+  console.log('adding note :', note)
   gNotes.unshift(note)
   utilService.saveToStorage('notes', gNotes)
 }
@@ -60,7 +56,7 @@ function _createNotes() {
 
   addNote(newTodoNote)
 
-  addNote(getEmptyTxtNote('txtGet a haircut} \n Live a little', 2 ))
+  addNote(getEmptyTxtNote('txtGet a haircut \n Live a little', 2 ))
 
   addNote(getEmptyTxtNote('Love me or leave me and let me be lonely \n You wont believe me but I love you only \n I\'d rather be lonley than happy with somebody else...', 4 ))
 
@@ -68,7 +64,7 @@ function _createNotes() {
 
   addNote(getEmptyTxtNote('O snail \n\tClimb Mount Fuji,   \nBut slowly, \n \t\t slowly!', 5))
 
-  addNote(getEmptyTxtNote('Go to the beach', 2))
+  addNote(getEmptyTxtNote('London Bridge is falling down, Falling down, falling down, London Bridge is falling down, My fair Lady.', 2))
 
   addNote(getEmptyTxtNote('O snail \n\tClimb Mount Fuji,   \nBut slowly, \n \t\t slowly!', 5))
 
@@ -111,10 +107,11 @@ function getEmptyTodoNote(txt = '', color) {
   }
 }
 
-function setNote(editedNote) {
+function updateNote(editedNote) {
   let id = editedNote.id
   let idx = getIdxById(id)
   gNotes[idx] = editedNote
+  gNotes[idx].id =  makeId()
   console.log('gNote[idx] :', gNotes[idx])
 }
 
